3078ef82b03a746b07173c1cfd2e8aa5676aaea2
Pads87 <paddingtonbear1987@hotmail.co.uk>
2025-06-01 23:07:11 +0100

    Add working Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,1 +1,22 @@
-// Tailwind CSS config
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        buddyblue: "#007bff",
+        lightgray: "#f7f7f7"
+      },
+      fontFamily: {
+        sans: ["Inter", "sans-serif"]
+      },
+      borderRadius: {
+        '2xl': '1rem'
+      }
+    }
+  },
+  plugins: []
+};
