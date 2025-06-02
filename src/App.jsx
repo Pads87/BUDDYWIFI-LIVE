661735db024af46065e15c92c02ef5cb545ffae7
Pads87 <paddingtonbear1987@hotmail.co.uk>
2025-06-02 15:33:05 +0100

    New Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage.jsx";
-import Dashboard from "./pages/Dashboard.jsx";
+import LandingPage from "./pages/LandingPage";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
