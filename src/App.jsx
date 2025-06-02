a96947167e74ee4cfa6820a5c20bf8397da6889a
Pads87 <paddingtonbear1987@hotmail.co.uk>
2025-06-02 14:51:45 +0100

    Fix getWalletAddress export issue for Vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import LandingPage from "./pages/LandingPage";
-import Dashboard from "./pages/Dashboard";
+import LandingPage from "./LandingPage";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col font-sans text-gray-900 bg-white">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
     </Router>
   );
 }
