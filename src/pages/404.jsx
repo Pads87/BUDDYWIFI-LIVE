7195095496770339111b98b766070403ffda4b6b
Pads87 <paddingtonbear1987@hotmail.co.uk>
2025-06-02 17:25:43 +0100

    New Update

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,22 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="description" content="BuddyWiFi is a decentralized mesh network powered by the community, allowing users to share and earn with Wi-Fi connectivity." />
-    <meta name="keywords" content="BuddyWiFi, WiFi sharing, decentralized network, mesh network, crypto internet, $BUDDY" />
-    <meta name="author" content="BuddyWiFi Team" />
-    <meta property="og:title" content="BuddyWiFi - Decentralized Wi-Fi Sharing" />
-    <meta property="og:description" content="Join the BuddyWiFi network and earn $BUDDY by sharing internet through a decentralized mesh." />
-    <meta property="og:image" content="/buddywifi-face.png" />
-    <meta property="og:url" content="https://www.buddywifi.com" />
-    <meta name="twitter:card" content="summary_large_image" />
-    <title>BuddyWiFi - Decentralized Wi-Fi Sharing Network</title>
-    <link rel="icon" type="image/x-icon" href="/favicon.ico" />
-  </head>
-  <body>
-    <div id="root"></div>
-    <script type="module" src="/src/main.jsx"></script>
-  </body>
-</html>
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center text-center p-4">
+      <h1 className="text-5xl font-bold mb-4">404</h1>
+      <p className="text-lg mb-6">Oops! The page you are looking for does not exist.</p>
+      <Link to="/" className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
+        Back to Home
+      </Link>
+    </div>
+  );
+};
+
+export default NotFound;
