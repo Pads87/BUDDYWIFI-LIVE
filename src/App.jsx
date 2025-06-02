b0dbc80867e36f956fc8a8012c12245cf4fe791c
Pads87 <paddingtonbear1987@hotmail.co.uk>
2025-06-02 16:16:30 +0100

    Updated routes and added landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/LandingPage";
-import Dashboard from "./pages/Dashboard";
+import { Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import AdminPanel from "./pages/AdminPanel.jsx";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/admin" element={<AdminPanel />} />
+    </Routes>
   );
 }
 
