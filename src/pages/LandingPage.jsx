3b0048071f045192b1b7337e9ac2dd04a6fcfad6
Pads87 <paddingtonbear1987@hotmail.co.uk>
2025-06-03 21:17:13 +0100

    New Update

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -5,26 +5,18 @@ import LiveMap from "../components/LiveMap";
 
 const LandingPage = () => {
   return (
-    <div className="landing-container animated-bg">
-      {/* Animated background video */}
-      <video autoPlay loop muted playsInline className="background-video">
+    <div className="animated-bg">
+      {/* Animated Video Background */}
+      <video
+        className="background-video"
+        autoPlay
+        loop
+        muted
+        playsInline
+      >
         <source src="/buddy-background.webm" type="video/webm" />
         Your browser does not support the video tag.
       </video>
-
-<div className="animated-bg">
-  <video
-    className="background-video"
-    autoPlay
-    loop
-    muted
-    playsInline
-  >
-    <source src="/buddy-background.webm" type="video/webm" />
-    Your browser does not support the video tag.
-  </video>
-</div>
-
 
       {/* Navigation Bar */}
       <nav className="navbar enhanced-navbar">
